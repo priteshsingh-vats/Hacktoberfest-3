15195c369550c15f7f3e3ff2a44649b7f34371b5
priteshsingh-vats <42402753+priteshsingh-vats@users.noreply.github.com>
2018-10-20 01:28:19 +0530

    Update test.c
    i have added some header files and codes to make program much more user freindly and better

--- test.c
+++ test.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 int main {
-	printf ("Seriously.. ?\n");
+	int i;
+	int j;
+	int sum;
+	printf("enter the number");
+	scanf("%d",&i);
+	printf("enter the second number");
+	scanf("%d",&j);
+	sum=i+j;
+	printf("sum is %d",sum);
+	printf ("Seriously....... ?\n");
+	return 0;
 }
